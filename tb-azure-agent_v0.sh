a0087e21d7015f0d7bc0b6420d9fa1fc832980e7
TeamBreakSas <82905835+TeamBreakSas@users.noreply.github.com>
2021-06-11 11:24:18 +0200

    Update tb-azure-agent_v0.sh

--- tb-azure-agent_v0.sh
+++ tb-azure-agent_v0.sh
@@ -7,7 +7,7 @@ fi
 
 sudo chown ubuntu:ubuntu $agent_folder
 
-config_serverurl="https://dev.azure.com/TeamBreakSas/DonutQuiz/"
+config_serverurl="https://dev.azure.com/TeamBreakSas/"
 config_pat_token="pncatiw5ud3d67ml7qzzrffppsxgrhgyzeq4fyubizxbyb5fut5a"
 config_agentname="$1"
 
